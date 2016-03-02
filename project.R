84bec59f4d5c060a8f721bfa868bda569347cc9c
SY Young <r38411@yahoo.com>
2016-03-02 21:50:03 +0800

    finish to the step 3

--- project.R
+++ project.R
@@ -1,30 +1,69 @@
-get_data <- function() {
+get_data <- function(filepath) {
+    print(filepath)
     # read the attribute list. it will be used as the column names
     fileName <- "./UCI HAR Dataset/features.txt"
     d1 <- read.table(fileName, colClasses=c("numeric","character"))
     feature_name <- d1[,2]
     
-    # read training set data
-    testFileName <- "./UCI HAR Dataset/test/X_test.txt"
+    # read set data
+    testFileName <- filepath["data"]
     data1 <- read.table(testFileName, colClasses=c("numeric"), col.names=feature_name)
-    print(str(data1))
     
-    # read training set data: subject
-    testFileName <- "./UCI HAR Dataset/test/subject_test.txt"
+    # read set data: subject
+    testFileName <- filepath["name"]
     d1 <- read.table(testFileName, colClasses=c("numeric"), col.names="volunteer")
     subject <- as.numeric(d1[,1])
-    print(str(subject))
     
-    testFileName <- "./UCI HAR Dataset/test/y_test.txt"
-    d1 <- read.table(testFileName, colClasses=c("numeric"), col.names="Activity")
+    testFileName <- filepath["act"]
+    d1 <- read.table(testFileName, colClasses=c("numeric"), col.names="activity")
     activity <- as.numeric(d1[,1])
-    print(str(activity))
     
     # integrate all three sets of data
-    testData <- cbind(subject,data1,activity)
-    print(str(testData))
+    allData <- cbind(subject,activity,data1)
+    
+    allData
 }
 
 project <- function() {
-    get_data()
+    # 1. step 0: read both test data set and training data set
+    # get the training data set
+    filepath <- c(name="./UCI HAR Dataset/train/subject_train.txt",
+                  data="./UCI HAR Dataset/train/X_train.txt",
+                  act="./UCI HAR Dataset/train/y_train.txt")
+    trainData <- get_data(filepath)
+    
+    # get the test data set
+    filepath <- c(name="./UCI HAR Dataset/test/subject_test.txt",
+                  data="./UCI HAR Dataset/test/X_test.txt",
+                  act="./UCI HAR Dataset/test/y_test.txt")
+    testData <- get_data(filepath)
+    
+    # 2. step 1: merges the training and test sets to creat one data set
+    wholeData <- rbind(trainData, testData)
+    
+    # 3. step 2: extracts only the measurements on mean/std for each measurement
+    library(dplyr)
+    wholeDatatf <- tbl_df(wholeData)
+    coredata <- select(wholeDatatf, subject, activity, contains("mean"))
+    coredata <- select(coredata, subject, activity, contains("Mag"))
+    cored2 <- select(wholeDatatf, contains("std"))
+    cored2 <- select(cored2, contains("Mag"))
+    coredata <- cbind(coredata, cored2)
+    print(str(coredata))
+    
+    # 4. step 3: uses descriptive activity names to name the activities
+    actList = c("walking","walking up", "walking down", "sitting",
+                "standing", "laying")
+    coredata <- mutate(coredata,activityname=actList[activity])
+    coredata <- select(coredata, -activity)
+    
+    # 5. step 5: create a 2nd independent tidy data set with the average of each
+    #   variable for each activity and each subject
+    indiv_data <- group_by(coredata,subject,activityname)
+    print(str(indiv_data))
+ 
+    summarise(indiv_data, totalMeasure=n(), mean_each=mean)
+    
+    indiv_data
+
 }
